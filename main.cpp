67dde4d4dadb29f7fa4b760cb20ef08109195f2c
Alina Ermoglio <alinaermoglio@eduroam-117-196.nomadic.bris.ac.uk>
2025-03-20 10:35:33 +0000

    Modified cpp file to produce different temperature values for different temperatures

--- main.cpp
+++ main.cpp
@@ -10,33 +10,47 @@
 using namespace std;
 
 int main() {
-    int n_configurations = 10000;        // number of configurations to create
-    
-    ofstream outfile("output1D.txt");   //creating file for output
+    int n_configurations = 1000;        // number of configurations to create
 
-    
     int N = 3;          // number of particles
-    int iterations = 50000;    //number of iterations for monte carlo
-    double T=3.5;        // we set k at 1, so our units for temperature have now changed to being dimensionless;
+    int iterations = 25000;    //number of iterations for monte carlo
+    // for this, we want to test a range of temperatures
+    double T_min=0.1, T_max=100;
+    double T_step=1;        //
 
+    ofstream energyFile("energy_vs_temperature1D.txt");
+    ofstream magFile("magnetisation_vs_temperature1D.txt");
+   
     srand(time(0));      // uses current time as the seed so all values produced for the states are different.
     
-    // parameters onto output file
-    outfile<<"Number of Configurations: "<<n_configurations<<endl; 
-    outfile<<"Number of Atoms: "<<N<<endl; 
-    outfile<<"Monte Carlo Iterations: "<<iterations<<endl; 
-    outfile<<"Temperature: "<<T<<endl; 
+    // Headers for file
+    energyFile<<"Temperature "<<"Average Energy"<<endl; 
+    magFile<<"Temperature "<<"Average Magnetistation"<<endl;
 
-    outfile<<"Configuration "<<"TotalEnergy "<<"Magnetisation "<<endl;   // output file header
-    for (int n=0;n<n_configurations;n++){
+    for (double T = T_min; T <= T_max; T += T_step){
+        double total_energy = 0.0;
+        double total_magnetisation = 0.0;
 
-        AtomChain chain(N);    //creating an object of class AtomChain
-        
-        MonteCarlo (chain,N,T,iterations);     //performing monte carlo on chain
-        // results are appended onto a file
-        outfile<<n<<" "<< chain.totalEnergy() <<" "<<chain.totalMagnetisation()<<endl;    // output onto file
+        for (int n=0;n<n_configurations;n++){
+
+            AtomChain chain(N);    //creating an object of class AtomChain
+            
+            MonteCarlo (chain,N,T,iterations); 
+
+            total_energy += chain.totalEnergy();
+            total_magnetisation += chain.totalMagnetisation();
+
+        }
+        double avg_energy = total_energy / n_configurations;
+        double avg_magnetisation = total_magnetisation / n_configurations;
+
+        energyFile << T << " " << avg_energy << endl;
+        magFile << T << " " << avg_magnetisation << endl;
+
+      // output onto file
 
     }
-    outfile.close();
+    energyFile.close();
+    magFile.close();
     return 0;
 }
