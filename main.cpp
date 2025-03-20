c48946ebbff54dbf5f80489093c1692b876f9c35
Alina Ermoglio <alinaermoglio@eduroam-117-196.nomadic.bris.ac.uk>
2025-03-20 09:15:08 +0000

    Playing around with parameters, will make a new branch to analyse the behaviour of energy and magnetisation with temperature, and leave the original code on the main branch to create histograms

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@
 using namespace std;
 
 int main() {
-    int n_configurations = 10000;        // number of configurations to create
+    int n_configurations = 5000;        // number of configurations to create
     
     ofstream outfile("output1D.txt");   //creating file for output
 
     
-    int N = 100;          // number of particles
+    int N = 3;          // number of particles
     int iterations = 50000;    //number of iterations for monte carlo
-    double T=0.034;        // we set k at 1, so our units for temperature have now changed to being dimensionless;
+    double T=100;        // we set k at 1, so our units for temperature have now changed to being dimensionless;
 
     srand(time(0));      // uses current time as the seed so all values produced for the states are different.
     
