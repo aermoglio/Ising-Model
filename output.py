bd8193d0774cfee0a4707521c4636254aab2c761
Alina Ermoglio <alinaermoglio@eduroam-117-196.nomadic.bris.ac.uk>
2025-03-20 09:27:01 +0000

    Refined histogram plots

--- output.py
+++ output.py
@@ -9,12 +9,16 @@ data=pd.read_csv(filename, skiprows=4, delimiter=" ")
 energy=data["TotalEnergy"]
 magnetisation = data["Magnetisation"]
 
-bins= 10
+bins= 4
 
 fig,ax1 = plt.subplots()
-ax1.hist(energy, bins=bins, color ="hotpink", edgecolor="black", alpha=0.7)
+ax1.hist(energy, bins=3, color ="mediumpurple", edgecolor="black", alpha=0.7)
+ax1.set_xlabel("Energy")
+ax1.set_ylabel("Frequency")
 
 fig,ax2 = plt.subplots()
-ax2.hist(magnetisation, bins=bins, color ="blue", edgecolor="black", alpha=0.7)
+ax2.hist(magnetisation, bins=bins, color ="paleturquoise", edgecolor="black", alpha=0.7)
+ax2.set_xlabel("Magnetisation")
+ax2.set_ylabel("Frequency")
 
 plt.show()
